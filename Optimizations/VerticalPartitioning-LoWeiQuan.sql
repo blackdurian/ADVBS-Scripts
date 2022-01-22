c551d6ca175a20457e48c46214750501abfe5186
lwqec <33524888+blackdurian@users.noreply.github.com>
2022-01-22 16:51:30 +0800

    Added Constraint to VerticalPartitioning

--- Optimizations/VerticalPartitioning-LoWeiQuan.sql
+++ Optimizations/VerticalPartitioning-LoWeiQuan.sql
@@ -2,7 +2,7 @@ USE TSI
 GO
 -- Create partition table,  flight table had divine into 2 vertically table
 CREATE TABLE FlightInfo(
-	FlightId INT NOT NULL PRIMARY KEY,
+	FlightId INT NOT NULL,
 	AircraftId INT NOT NULL REFERENCES Aircraft(Id),
 	Code VARCHAR(50) NOT NULL,
 	FlightState VARCHAR(50) NOT NULL,
@@ -11,11 +11,13 @@ CREATE TABLE FlightInfo(
 	ArrivalTime DATETIMEOFFSET(4) NOT NULL,
 	NumOfMeal INT NOT NULL,
 	HasUnaccompaniedMinorService BIT NOT NULL,
-	HasWheelchairService BIT NOT NULL
+	HasWheelchairService BIT NOT NULL,
+	CONSTRAINT PK_FlightInfo PRIMARY KEY CLUSTERED (FlightId),
+	CONSTRAINT FK_FlightInfo FOREIGN KEY (FlightId) REFERENCES Flight(id)
  )
 GO
  CREATE TABLE FlightCharge( 
-	FlightId INT NOT NULL PRIMARY KEY,
+	FlightId INT NOT NULL,
 	FirstClassPrice MONEY NOT NULL,
 	BusinessClassPrice MONEY NOT NULL,
 	PremiumClassPrice MONEY NOT NULL,
@@ -23,7 +25,13 @@ GO
 	CancelFeeChargeRate DECIMAL (5,4) NOT NULL CHECK (CancelFeeChargeRate >= 0 and CancelFeeChargeRate < 1) , --(CancelFeeChargeRate between 0 and 1)
 	ChangeFeeChargeRate  DECIMAL (5,4) NOT NULL CHECK (ChangeFeeChargeRate >= 0 and ChangeFeeChargeRate < 1) , --(ChangeFeeChargeRate between 0 and 1)
 	InfantChargeRate DECIMAL (5,4) NOT NULL CHECK (InfantChargeRate >= 0 and InfantChargeRate < 1) --(InfantChargeRate between 0 and 1)
+	CONSTRAINT PK_FlightCharge PRIMARY KEY CLUSTERED (FlightId),
+	CONSTRAINT FK_FlightCharge FOREIGN KEY (FlightId) REFERENCES Flight(id)
  )
+GO
+BEGIN TRAN
+GO
+SET NOCOUNT ON
 GO
 INSERT FlightInfo (FlightId, AircraftId, Code, FlightState, RouteId, DepartureTime, ArrivalTime,
 	NumOfMeal, HasUnaccompaniedMinorService, HasWheelchairService) 
@@ -35,5 +43,7 @@ INSERT FlightCharge(FlightId, FirstClassPrice, BusinessClassPrice, PremiumClassP
 SELECT Id, FirstClassPrice, BusinessClassPrice, PremiumClassPrice, EconomyClassPrice,
 	CancelFeeChargeRate, ChangeFeeChargeRate, InfantChargeRate FROM Flight
 GO
-
-
+SET NOCOUNT OFF
+GO
+COMMIT TRAN
+GO
